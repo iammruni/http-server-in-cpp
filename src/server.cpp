6c9fd0bff5e3566f5784f3d6a5647395085bd30d
iammruni <mrunal.s15@gmail.com>
2025-02-16 21:00:58 +0530

    refactored; created req resp structs and added file write post req

--- src/server.cpp
+++ src/server.cpp
@@ -10,7 +10,32 @@
 #include <thread>
 #include <fstream>
 #include <sstream>
+#include <map>
+#include <vector>
 using namespace std;
+
+
+// Define header map
+using header_map = map<string, string>;
+
+// Struct for request
+struct request {
+  string method;
+  string path;
+  string version;
+  header_map headers;
+  string body;
+};
+
+// Struct for response
+struct response {
+  string version = "HTTP/1.1";
+  int status_code;
+  string status_msg;
+  header_map headers;
+  string body;
+};
+
 
 string extract_info(const string& request, const string to_find, const string until) {
   // Find the position of the first space after GET
@@ -25,59 +50,141 @@ string extract_info(const string& request, const string to_find, const string un
   return url;
 }
 
+// Function to parse headers from request
+void parse_headers(const string& request, header_map& headers) {
+  size_t header_start = request.find("\r\n") + 2;  // Skip first line (method, path, version)
+  size_t header_end = request.find("\r\n\r\n");   // End of headers
+
+  string headers_part = request.substr(header_start, header_end - header_start);
+
+  stringstream ss(headers_part);
+  string line;
+  while (getline(ss, line)) {
+      size_t colon_pos = line.find(": ");
+      if (colon_pos != string::npos) {
+          string key = line.substr(0, colon_pos);
+          string value = line.substr(colon_pos + 2);
+          headers[key] = value;
+      }
+  }
+}
+
+// Function to parse the request data
+request parse_request(const string& request_str) {
+  request req;
+  
+  // Parse the request line
+  req.method = extract_info(request_str, "", " ");
+  req.path = extract_info(request_str, req.method + " ", " ");
+  req.version = extract_info(request_str, req.method + " " + req.path + " ", "\r\n");
+  
+  // Parse headers
+  parse_headers(request_str, req.headers);
+  
+  // Parse the body (if any)
+  size_t body_start = request_str.find("\r\n\r\n") + 4;  // Skip headers
+  if (body_start != string::npos) {
+      req.body.assign(request_str.begin() + body_start, request_str.end());
+  }
+
+  return req;
+}
+
+
+// Function to create a response
+string create_response(const response& resp) {
+  stringstream ss;
+  ss << resp.version << " " << resp.status_code << " " << resp.status_msg << "\r\n";
+  
+  // Add headers
+  for (const auto& header : resp.headers) {
+      ss << header.first << ": " << header.second << "\r\n";
+  }
+  
+  ss << "\r\n";  // End of headers
+  
+  // Add body
+  ss << resp.body;
+  return ss.str();
+}
+
 void handleClient (int client, int argc, char** argv) {
 
   char receivebuf[1024];
   int receivedbytes = recv(client, receivebuf, sizeof(receivebuf) - 1, 0);
   receivebuf[receivedbytes] = '\0';
-  string request(receivebuf);
+  string request_str(receivebuf);
 
-  string url = extract_info(receivebuf, "GET ", " ");
-  string usr_agent = extract_info(receivebuf, "User-Agent: ", "\r\n");
+  // Parse req
+  request req = parse_request(request_str);
+
+  response resp;
+
   string reply = "";
 
-  if (url == "/") {
-    reply = "HTTP/1.1 200 OK\r\n\r\n";
-  }
-  else if (url.starts_with("/user-agent")) {
-    reply = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
-                   + std::to_string(usr_agent.length()) 
-                   + "\r\n\r\n" 
-                   + usr_agent;
-  }
-  else if (url.starts_with("/echo")) {
-    string echocontent = url.substr(6, url.length());
-    reply = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
-                   + std::to_string(echocontent.length()) 
-                   + "\r\n\r\n" 
-                   + echocontent;
-  }
-  else if (url.starts_with("/files")) {
-    string file = url.substr(7, url.length());
-    if (argc >= 3) {
-      if (string(argv[1]) == "--directory") {
-        file = argv[2] + file;
+  if (req.method == "GET"){
+    if (req.path == "/") {
+      resp.status_code = 200;
+      resp.status_msg = "OK";
+    }
+    else if (req.path.starts_with("/user-agent")) {
+      resp.status_code = 200;
+      resp.status_msg = "OK";
+      resp.headers["Content-Type"] = "text/plain";
+      resp.headers["Content-Length"] = to_string(req.headers["User-Agent"].length());
+      resp.body = req.headers["User-Agent"].c_str();
+    }
+    else if (req.path.starts_with("/echo")) {
+      string echocontent = req.path.substr(6, req.path.length());
+      resp.status_code = 200;
+      resp.status_msg = "OK";
+      resp.headers["Content-Type"] = "text/plain";
+      resp.headers["Content-Length"] = to_string(echocontent.length());
+      resp.body = echocontent;
+    }
+    else if (req.path.starts_with("/files")) {
+      string file = req.path.substr(7, req.path.length());
+      if (argc >= 3) {
+        if (string(argv[1]) == "--directory") {
+          file = argv[2] + file;
+        }
+      }
+      ifstream myfile(file);
+      if (myfile.is_open()) {
+        ostringstream oss;
+        oss << myfile.rdbuf();
+        resp.body = oss.str();
+        resp.status_code = 200;
+        resp.status_msg = "OK";
+        resp.headers["Content-Type"] = "application/octet-stream";
+        resp.headers["Content-Length"] = to_string(resp.body.length());
+      } else {
+        resp.status_code = 404;
+        resp.status_msg = "Not Found";
       }
     }
-    ifstream myfile(file);
-    if (myfile.is_open()) {
-      ostringstream oss;
-      oss << myfile.rdbuf();
-      string content;
-      content = oss.str();
-      reply = "HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " 
-                   + std::to_string(content.length()) 
-                   + "\r\n\r\n" 
-                   + content;
-    } else {
-      reply = "HTTP/1.1 404 Not Found\r\n\r\n";
+    else {
+      resp.status_code = 404;
+      resp.status_msg = "Not Found";
+    }
+  } else if (req.method == "POST") {
+    if (req.path.starts_with("/files/")) {
+      string file_name = req.path.substr(7, req.path.length());
+      if (argc >= 3) {
+        if (string(argv[1]) == "--directory") {
+          file_name = argv[2] + file_name;
+        }
+      }
+      ofstream wfile(file_name);
+      wfile << req.body.c_str();
+      wfile.close();
+      resp.status_code = 201;
+      resp.status_msg = "Created";
     }
   }
-  else {
-    reply = "HTTP/1.1 404 Not Found\r\n\r\n";
-  }
-  send(client, reply.c_str(), reply.length(), 0);
 
+  string response_str = create_response(resp);
+  send(client, response_str.c_str(), response_str.length(), 0);
   close(client);
 
 }
